e148c02984b51bb7d6665dfcda9cf5987bacec93
Miika Hänninen <miika.hanninen@reaktor.com>
2017-12-06 21:54:50 +0200

    Add solution for day 1 part 2

--- 01/src/Main.purs
+++ 01/src/Main.purs
@@ -1,10 +1,10 @@
 module Main where
 
-import Prelude (Unit, bind, map, otherwise, pure, show, (#), ($), (==))
+import Prelude (Unit, bind, map, otherwise, pure, show, (#), ($), (==), (<>), (/), discard)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Data.List (List(..), fromFoldable, snoc, zip, (:))
+import Data.List (List(..), fromFoldable, snoc, zip, (:), drop, take, length)
 import Data.Traversable (sum)
 import Data.Tuple (Tuple(..))
 import Data.String (toCharArray)
@@ -18,6 +18,10 @@ shiftByOne :: forall a. List a -> List a
 shiftByOne Nil = Nil
 shiftByOne (x:xs) = snoc xs x
 
+shiftByHalf :: forall a. List a -> List a
+shiftByHalf Nil = Nil
+shiftByHalf xs = (drop (length xs / 2) xs) <> (take (length xs / 2) xs)
+
 pairValue :: Tuple Int Int -> Int
 pairValue (Tuple a b) | a == b = a
                       | otherwise = 0
@@ -25,6 +29,9 @@ pairValue (Tuple a b) | a == b = a
 solution :: List Int -> Int
 solution Nil = 0
 solution xs = sum $ map pairValue $ zip xs (shiftByOne xs)
+
+solution2 :: List Int -> Int
+solution2 xs = sum $ map pairValue $ zip xs (shiftByHalf xs)
 
 charToInt :: Char -> Maybe Int
 charToInt '0' = pure 0
@@ -47,5 +54,7 @@ parseInput x = toCharArray x
 
 main :: forall e. Eff (exception :: EXCEPTION, fs :: FS, console :: CONSOLE | e) Unit
 main = do
-    input <- readTextFile UTF8 "input" 
-    log $ show $ solution $ parseInput input
+    input <- readTextFile UTF8 "input"
+    let values = parseInput input
+    log $ show $ solution values
+    log $ show $ solution2 values
--- 01/test/Main.purs
+++ 01/test/Main.purs
@@ -4,12 +4,17 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Test.Assert
-import Main (solution)
+import Main (solution, solution2)
 import Data.List
 
 assertCase :: forall e. Int -> Int -> Array Int -> Eff (assert :: ASSERT | e) Unit
 assertCase num expected values = do
     let result = solution $ fromFoldable values
+    assert' ("Case "<>show num<>" failed, expected "<>show expected<>", actual "<>show result) (result == expected)
+
+assertCase2 :: forall e. Int -> Int -> Array Int -> Eff (assert :: ASSERT | e) Unit
+assertCase2 num expected values = do
+    let result = solution2 $ fromFoldable values
     assert' ("Case "<>show num<>" failed, expected "<>show expected<>", actual "<>show result) (result == expected)
 
 main :: forall e. Eff (assert :: ASSERT | e) Unit
@@ -19,3 +24,9 @@ main = do
     assertCase 2 4 [1,1,1,1]
     assertCase 3 0 [1,2,3,4]
     assertCase 4 9 [9,1,2,1,2,1,2,9]
+    assertCase2 0 0 []
+    assertCase2 1 6 [1,2,1,2]
+    assertCase2 2 0 [1,2,2,1]
+    assertCase2 3 4 [1,2,3,4,2,5]
+    assertCase2 4 12 [1,2,3,1,2,3]
+    assertCase2 5 4 [1,2,1,3,1,4,1,5]
