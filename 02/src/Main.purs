79518c21c901caffe7d1ed528f102e2094665f8a
Miika Hänninen <miika.hanninen@reaktor.com>
2017-12-08 14:37:18 +0200

    Add solution for day 2 part 2

--- 02/src/Main.purs
+++ 02/src/Main.purs
@@ -1,10 +1,11 @@
 module Main where
 
-import Prelude (Unit, bind, map, otherwise, not, pure, show, (#), ($), (==), (-),(<<<))
+import Prelude (Unit, bind, map, otherwise, not, pure, show, (#), ($), (==), (-),(<<<), discard, (/=), mod, (/))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Data.List (List(..), filter, fromFoldable, snoc, zip, (:))
+import Control.MonadZero (guard)
+import Data.List (List(..), filter, fromFoldable, head, snoc, zip, (:))
 import Data.Traversable (sum, maximum, minimum)
 import Data.Tuple (Tuple(..))
 import Data.String (toCharArray, split, null, Pattern(..))
@@ -20,8 +21,23 @@ import Partial.Unsafe (unsafePartial)
 rowValue :: List Int -> Int
 rowValue x = (fromMaybe 0 (maximum x)) - (fromMaybe 0 (minimum x))
 
+findEvenDivisions :: List Int -> List Int
+findEvenDivisions xs = do
+    x <- xs
+    y <- xs
+    guard (x /= y)
+    guard (x `mod` y == 0)
+    pure (x / y)
+
+rowValue2 :: List Int -> Int
+rowValue2 Nil = 0
+rowValue2 xs = unsafePartial $ fromJust $ head $ findEvenDivisions xs
+
 solution :: List (List Int) -> Int
 solution = sum <<< (map rowValue)
+
+solution2 :: List (List Int) -> Int
+solution2 = sum <<< (map rowValue2)
 
 lines :: String -> List String
 lines = filter (not <<< null) <<< fromFoldable <<< split (Pattern "\n")
@@ -38,4 +54,6 @@ parseInput x = map (wordsToInts <<< words) $ lines x
 main :: forall e. Eff (exception :: EXCEPTION, fs :: FS, console :: CONSOLE | e) Unit
 main = do
     input <- readTextFile UTF8 "input" 
-    log $ show $ solution $ parseInput input
+    let values = parseInput input
+    log $ show $ solution values
+    log $ show $ solution2 values
